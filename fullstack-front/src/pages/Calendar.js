6bf40a0a1ba647d74bfa1971bf3654f55d56dd54
Avry Jackson <158083847+avryjacksoncom@users.noreply.github.com>
2025-03-11 13:03:27 -0700

    create women's in stem club template site

--- fullstack-front/src/pages/Calendar.js
+++ fullstack-front/src/pages/Calendar.js
@@ -4,7 +4,18 @@ export default function Calendar() {
 
     return (
         <div className="App">
-           
+                {/* Calendar Section */}
+                <div className="w-full bg-white shadow flex flex-col my-4 p-6">
+                        <p className="text-xl font-semibold pb-5">Event Calendar</p>
+                        <iframe
+                            title="Furry Events Calendar"
+                            src="https://calendar.google.com/calendar/u/0/embed?src=morernos355@gmail.com&ctz=America/Los_Angeles"
+                            
+                            className="w-full h-96 border"
+                            style={{ height: '700px', width: '100%' }}
+                            frameBorder="0"
+                        ></iframe>
+                    </div>
         </div>
     );
 }
